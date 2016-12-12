5983b9209c740077fcb59b58e32bfadef11a5b07
Sam <sam@sraxrt.me>
2016-12-12 17:11:21 +0100

    first commit

---
+++ blockNumber/README.md
@@ -0,0 +1,7 @@
+# BlockNumber
+
+Script to get the number of last block though JSON-RPC  
+  
+Prerequisites :  
+- Ethereum node (geth ou Parity), with JSON6RPC enabled
+  
---
+++ blockNumber/getLastBlockNumber.py
@@ -0,0 +1,10 @@
+import requests
+import json
+
+host = '192.168.0.49'
+host = 'http://' + host + ':8545'
+headers = {'Content-Type': 'application/json',}
+data = '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}'
+result = requests.post(host, headers=headers, data=data)
+
+parsed_json = json.loads(result.text)
