41e3003efaf0ed72fb2f9cde762c52d8802b605c
Sam <SamR1@users.noreply.github.com>
2017-03-29 16:13:16 +0200

    Update getLastBlockNumber.py

--- blockNumber/getLastBlockNumber.py
+++ blockNumber/getLastBlockNumber.py
@@ -6,5 +6,6 @@ host = 'http://' + host + ':8545'
 headers = {'Content-Type': 'application/json',}
 data = '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}'
 result = requests.post(host, headers=headers, data=data)
+parsed_json = json.loads(result.text)
 
 print(int(parsed_json['result'], 0))
